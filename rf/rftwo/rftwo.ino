56dbdb52c27142f6025b24b80eb00d39858f390e
KyomaHooin <richard_bruna@nm.cz>
2016-05-31 14:04:34 +0200

    Bare minimum.

--- rf/rftwo/rftwo.ino
+++ rf/rftwo/rftwo.ino
@@ -14,35 +14,19 @@
 //RF24  class instance na pinech CE/CSN.
 RF24 rf(RF_CE, RF_CSN);
 //RF24 data RX/TX pipe address:
-//const uint64_t rf_rx_pipe = 0xE8E8F0F0E1LL;
-byte addresses[][6] = {"1Node","2Node"};
-//RF debug
-static FILE uartout = {0};
-static int uart_putchar (char c, FILE *stream) {
-  Serial.write(c);
-  return 0;
-}
+const uint64_t rf_rx_pipe = 0xE8E8F0F0E1LL;
 
 //SETUP
 
 void setup() {
-  //DEBUG setup
+  //DEBUG
   Serial.begin(9600);
-  // fill in the UART file descriptor with pointer to writer.
-  fdev_setup_stream (&uartout, uart_putchar, NULL, _FDEV_SETUP_WRITE);
-  // The uart is the standard output device STDOUT.
-  stdout = &uartout ;
-  //RF SETUP
-  //Wakeup LEVEL 1
+  //Wakeup level(1)
   rf.begin();
-  //Set power level
+  //Set PA level
   rf.setPALevel(RF24_PA_LOW);
-  //  Serial.println("Geting status...");
-  rf.printDetails();
   //Open RX pipe
-//  rf.openReadingPipe(1, rf_rx_pipe);
-  rf.openWritingPipe(addresses[0]);
-  rf.openReadingPipe(1,addresses[1]);
+  rf.openReadingPipe(1, rf_rx_pipe);
   //Open RX queue
   rf.startListening();
 }
@@ -50,12 +34,15 @@ void setup() {
 //MAIN
 
 void loop() {
-    unsigned long got_time;
+  //payload
+    float payload;
+    //read payload
     if( rf.available()){
       while (rf.available()) {
-        rf.read( &got_time, sizeof(unsigned long) );
+        rf.read(&payload, sizeof(float));
       }
-      Serial.print(F("Got response: "));
-      Serial.println(got_time);  
+      Serial.println(payload);
    }
+   //Queue delay
+   delay(1000);
 }
