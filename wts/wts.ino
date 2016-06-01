26f4664b7c76b8550e577e25e72417348281be2f
KyomaHooin <richard_bruna@nm.cz>
2016-06-01 18:36:26 +0200

    Final.

--- wts/wts.ino
+++ wts/wts.ino
@@ -2,6 +2,14 @@
 /////////////////////////////////////////////////
 // Wireless thermometr server by Richard Bruna //
 /////////////////////////////////////////////////
+//
+//vccRead() by Scott "dky" 
+//
+//#define PIN_DC 8 //data/command switch -> PCD8544_SPI_FB.h
+//#define PIN_RESET 9 //reset switch -> PCD8544_SPI_FB.h
+//#define RF_MOSI 11 -> Hardwared MOSI SPI(10 Mhz)
+//#define RF_MISO 12 -> Hardwired MISO SPI(10 Mhz)
+//#define RF_SCK 13  -> Hardwired SCK SPI (10 Mhz)
 
 #include <SPI.h>
 #include <RF24.h>
@@ -9,14 +17,12 @@
 #include </root/sketchbook/wtemp/nokia/font.h>
 #include <LowPower.h>
 
-#define RF_CE 8 //RX/TX mode
-#define RF_CSN 9 //SS SPI
-
-#define PIN_SCE 10// SS -> PCD8544_SPI_FB.h
-#define PIN_SDIN 11// MOSI Hardwired SPI(4Mhz)
-#define PIN_SCLK 13// SCK Hardwired SPI(4Mhz)
-#define USE_FRAME_BUFFER// pre-buffered LCD output
-#define LCD_POWER 2
+#define RF_CE 5 //RF RX/TX mode
+#define RF_CSN 6 //RF SS SPI
+#define PIN_SCE 10// LCD SS -> PCD8544_SPI_FB.h
+#define PIN_SDIN 11// LCD MOSI Hardwired SPI(4Mhz)
+#define PIN_SCLK 13// LCD SCK Hardwired SPI(4Mhz)
+#define USE_FRAME_BUFFER//LCD pre-buffered LCD output
 
 //RF24  class instance na pinech CE/CSN.
 RF24 rf(RF_CE, RF_CSN);
@@ -28,23 +34,24 @@ struct payloadStruct {
   float temperature;
 } payload;
 //create frame buffered instance
-//PCD8544_SPI_FB lcd;
+PCD8544_SPI_FB lcd;
 
+//RF sync counter
 unsigned long rf_timeout;
-float vref = 3.1;
-float temperature = 25.56;
 
 //SETUP
 
 void setup() {
   //DEBUG
   Serial.begin(9600);
-  //Wakeup level(1)
+  //RF Wakeup level(1)
   rf.begin();
-  //Set PA level
+  //RF PA level
   rf.setPALevel(RF24_PA_LOW);
-  //Open RX pipe
+  //RF open RX pipe
   rf.openReadingPipe(1, rf_rx_pipe);
+  //LCD setup
+  lcd.begin(0, 0xBD, 0x04, 0x13); //invert, contrast, tempcoef, bias
 }
 
 //MAIN
@@ -56,28 +63,36 @@ void loop() {
   rf.startListening();
   //RF timeout counter
   rf_timeout = millis();
-  //RF sync(max. = sleep time) => time to sync 0-10ms
+  //RF sync max. sleep time
   while(!rf.available()) {
     if (millis() - rf_timeout > 1000 ) { break; }
   };
-  Serial.print("Delay: ");
-  Serial.println(millis() - rf_timeout);
-  //RF get payload
+  //RF get payload.
   if (rf.available()){
     rf.read(&payload, sizeof(payload));
+    rf.stopListening();
+    rf.powerDown();
+    //Display payload.
+    lcd_display_voltage(payload.voltage, float(readVcc())/1000);
+    lcd_display_temperature(payload.temperature);
+    lcd.renderAll();
     Serial.println(payload.voltage);
     Serial.println(payload.temperature);
-  } else { Serial.println("E"); }
-  //RF stop
-  rf.stopListening();
-  rf.powerDown();
- //sleep goes here..
- delay(5095);//4000 + 750 DS conversion delay(!) + 340 PowerUp delay(!)
+  } else {
+    //Display error.
+    rf.stopListening();
+    rf.powerDown();
+    lcd.clear();
+    lcd_display_error();
+    lcd.renderAll();
+  }
+  //sleep goes here..
+  sleep();
+  delay(500); //Sleep 4000 + 500 WakeUp delay
 }
 
 long readVcc() {
-  // Read 1.1V reference against AVcc 
-  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  // Read 1.1V reference against AVcc, set the reference to Vcc and the measurement to the internal 1.1V reference
   ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
   delay(2); // Wait for Vref to settle
   ADCSRA |= _BV(ADSC); // Start conversion
@@ -90,46 +105,48 @@ long readVcc() {
 }
 
 //Get single 3x14 digit PROGMEM bitmap.
-//void lcd_font(byte i, byte x){
-//  uint8_t buff[42];
-//  memcpy_P(buff, font[i], 42);
-//  lcd.writeBitmap(buff, x, 2, 14, 3);
-//}
+void lcd_font(byte i, byte x){
+  uint8_t buff[42];
+  memcpy_P(buff, font[i], 42);
+  lcd.writeBitmap(buff, x, 2, 14, 3);
+}
 
 //Get 2x10 thermometr logo PROGMEM bitmap.
-//void lcd_thermometr(){
-//  uint8_t buff[20];
-//  memcpy_P(buff, thermo, 20);
-//  lcd.writeBitmap(buff, 74, 2, 10, 2);
-//}
+void lcd_thermometr(){
+  uint8_t buff[20];
+  memcpy_P(buff, thermo, 20);
+  lcd.writeBitmap(buff, 74, 2, 10, 2);
+}
 
 //Display server/client voltage reference.
-//void lcd_display_voltage(float server_vcc, float client_vcc) {
-//  lcd.print(server_vcc);
-//  lcd.gotoXY(18, 0);
-//  lcd.print("V");
-//  lcd.gotoXY(60, 0);
-//  lcd.print(client_vcc);
-//  lcd.gotoXY(78, 0);
-//  lcd.print("V");
-//}
+void lcd_display_voltage(float server_vcc, float client_vcc) {
+  lcd.print(server_vcc);
+  lcd.gotoXY(24, 0);
+  lcd.print("V");
+  lcd.gotoXY(54, 0);
+  lcd.print(client_vcc);
+  lcd.gotoXY(78, 0);
+  lcd.print("V");
+}
 
 //Display temperature digits.
-//void lcd_display_temperature(float f) {
-//  if (f < 0) { lcd_font(10,0); } // "Negative One" ;).. 
-//  if (abs(int(f) / 10) > 0) { lcd_font(abs(int(f) / 10), 15); }
-//  lcd_font(abs(int(f) % 10), 30);
-//  lcd_font(11, 43); //Precision point.
-//  lcd_font(abs(int(f * 10.0) % 10), 56);
-//  lcd_thermometr();
-//}
+void lcd_display_temperature(float f) {
+  if (f < 0) { lcd_font(10,0); } // "Negative One" ;).. 
+  if (abs(int(f) / 10) > 0) { lcd_font(abs(int(f) / 10), 15); }
+  lcd_font(abs(int(f) % 10), 30);
+  lcd_font(11, 43); //Precision point.
+  lcd_font(abs(int(f * 10.0) % 10), 56);
+  lcd_thermometr();
+}
 
 //Display error.
-//void lcd_display_error() {
-//  lcd_font(12, 56);
-//  lcd_thermometr();
-//}
+void lcd_display_error() {
+  lcd.gotoXY(18, 2);
+  lcd.print("NO DATA!");
+  //lcd_thermometr();
+}
 
+//Sleep for 4s.
 void sleep(){
   LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);  
 }
