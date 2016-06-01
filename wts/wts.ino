9df877ef3c1bd0007ccf1b8970907fedff850507
KyomaHooin <richard_bruna@nm.cz>
2016-06-01 15:12:38 +0200

    Beta.

--- wts/wts.ino
+++ wts/wts.ino
@@ -6,150 +6,130 @@
 #include <SPI.h>
 #include <RF24.h>
 #include <PCD8544_SPI.h>
+#include </root/sketchbook/wtemp/nokia/font.h>
 #include <LowPower.h>
-#include <stdio.h>
 
-//VAR
+#define RF_CE 8 //RX/TX mode
+#define RF_CSN 9 //SS SPI
 
+#define PIN_SCE 10// SS -> PCD8544_SPI_FB.h
+#define PIN_SDIN 11// MOSI Hardwired SPI(4Mhz)
+#define PIN_SCLK 13// SCK Hardwired SPI(4Mhz)
+#define USE_FRAME_BUFFER// pre-buffered LCD output
 #define LCD_POWER 2
-#define RF_POWER 3
-#define RF_CE 6 //RX/TX mode
-#define RF_CSN 7 //SPI SS
-//#define RF_MOSI 11 -> Hardwared MOSI SPI(10 Mhz)
-//#define RF_MISO 12 -> Hardwired MISO SPI(10 Mhz)
-//#define RF_SCK 13 -> Hardwired SCK SPI(10 Mhz)
-//#define PIN_DC 8 data/command switch -> PCD8544_SPI_FB.h
-//#define PIN_RESET 9 reset switch -> PCD8544_SPI_FB.h
-#define PIN_SCE 10 //SPI SS -> PCD8544_SPI_FB.h
-//#define PIN_SDIN 11 -> MOSI Hardwired SPI(4Mhz)
-//#define PIN_SCLK 13 -> SCK Hardwired SPI(4Mhz)
-//#define LED_POWER 13 -> SCK SPI
-//5110 buffered mode
-#define USE_FRAME_BUFFER
-//Create RF24 class instance
+
+//RF24  class instance na pinech CE/CSN.
 RF24 rf(RF_CE, RF_CSN);
-//5110 LCD class instance
-PCD8544_SPI_FB lcd;
-//RF24 data RX pipe address
-const uint64_t rf_pipe_address = 0xF0F0F0F0D2LL;
-//5110 LCD size
-//const byte LCD_WIDTH = 84;
-//const byte LCD_HEIGHT = 48;
+//RF24 data RX/TX pipe address:
+const uint64_t rf_rx_pipe = 0xE8E8F0F0E1LL;
+//RF payload struct
+struct payloadStruct {
+  float voltage;
+  float temperature;
+} payload;
+//create frame buffered instance
+//PCD8544_SPI_FB lcd;
 
-// A bitmap graphic (10x2) of a thermometer...
-static const byte THERMO_WIDTH = 10;
-static const byte THERMO_HEIGHT = 2;
-//Temperature symbol
-static const byte thermometer[] = {0x00, 0x00, 0x48, 0xfe, 0x01, 0xfe, 0x00, 0x02, 0x05, 0x02,
-                                   0x00, 0x00, 0x62, 0xff, 0xfe, 0xff, 0x60, 0x00, 0x00, 0x00};                                    
-//DEBUG
-
-// create a FILE structure to reference our UART output function
-static FILE uartout = {0};
-// create a output function
-   // This works because Serial.write, although of
-   // type virtual, already exists.
-static int uart_putchar (char c, FILE *stream) {
-  Serial.write(c);
-  return 0;
-}
+unsigned long rf_timeout;
+float vref = 3.1;
+float temperature = 25.56;
 
 //SETUP
 
 void setup() {
-//DEBUG setup
+  //DEBUG
   Serial.begin(9600);
-// fill in the UART file descriptor with pointer to writer.
-  fdev_setup_stream (&uartout, uart_putchar, NULL, _FDEV_SETUP_WRITE);
-// The uart is the standard output device STDOUT.
-  stdout = &uartout ;
-  
-//RF setup
-  pinMode(RF_POWER, OUTPUT);
-  digitalWrite(RF_POWER, LOW);
-  pinMode(RF_CSN, OUTPUT);
-  digitalWrite(RF_CSN, HIGH); //SS default disable
-//5110 setup
-  pinMode(LCD_POWER, OUTPUT);
-  digitalWrite(LCD_POWER, LOW);
-  pinMode(PIN_CE, OUTPUT);
-  digitalWrite(PIN_CE, HIGH); //SS default disable
-//LED setup
-//  pinMode(LED_POWER, OUTPUT);
-//  digitalWrite(LED_POWER, LOW);
+  //Wakeup level(1)
+  rf.begin();
+  //Set PA level
+  rf.setPALevel(RF24_PA_LOW);
+  //Open RX pipe
+  rf.openReadingPipe(1, rf_rx_pipe);
 }
 
-//LOOP
+//MAIN
 
 void loop() {
-  //rf_up();
-  //rf_status();
-  lcd_up();
-  //float temp = rf_receive();
-  //Serial.println(temp);
-  lcd_display();
-  //rf_down();
-  //lcd_down();
-  //arduino_down();
-  delay(10000);
-}
-
-//FUNCTION
-
-void rf_up() {
-  digitalWrite(RF_POWER, HIGH);
-  rf.begin();
-  rf.setPALevel(RF24_PA_LOW);
-  rf.openReadingPipe(1,rf_pipe_address);//RX pipe
-}
-
-void rf_status() {
-  if(digitalRead(PIN_SCE) == HIGH) {
-    rf.printDetails();
-  }
-}
-
-float rf_receive() {
-  float buff;
-  if(digitalRead(PIN_SCE) == HIGH) {
-    rf.startListening();
-    while (rf.available()) {
-      rf.read(&buff, sizeof(float));
-    }
-    rf.stopListening();
- } 
- return buff;
-}
-
-void rf_down() {
+  //RF start
+  rf.powerUp();
+  //Listen
+  rf.startListening();
+  //RF timeout counter
+  rf_timeout = millis();
+  //RF sync(max. = sleep time) => time to sync 0-10ms
+  while(!rf.available());
+  Serial.print("Delay: ");
+  Serial.println(millis() - rf_timeout);
+  //RF get payload
+  if (rf.available()){
+    rf.read(&payload, sizeof(payload));
+    Serial.println(payload.voltage);
+    Serial.println(payload.temperature);
+  } else { Serial.println("E"); }
+  //RF stop
+  rf.stopListening();
   rf.powerDown();
-  digitalWrite(RF_POWER, LOW);
+ //sleep goes here..
+ //sleep();
+ delay(4000);
 }
 
-void lcd_up() {
-  digitalWrite(LCD_POWER, HIGH);
-  //setup invert,tempCoef,bias, 
-  //lcd.begin(false,0xB1,0x04,0xB1);
-  lcd.begin();
-}
+//long readVcc() {
+//  // Read 1.1V reference against AVcc 
+//  // set the reference to Vcc and the measurement to the internal 1.1V reference
+//  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+//  delay(2); // Wait for Vref to settle
+//  ADCSRA |= _BV(ADSC); // Start conversion
+//  while (bit_is_set(ADCSRA,ADSC)); // measuring
+//  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH
+//  uint8_t high = ADCH; // unlocks both
+//  long result = (high<<8) | low;
+//  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+//  return result; // Vcc in millivolts
+//}
 
-void lcd_display() {
-  if(digitalRead(RF_CSN) == HIGH) {
-  lcd.clear();
-  lcd.writeBitmap(thermometer, 0, 2, THERMO_WIDTH, THERMO_HEIGHT);
-  lcd.renderAll();
-  }
-}
+//Get single 3x14 digit PROGMEM bitmap.
+//void lcd_font(byte i, byte x){
+//  uint8_t buff[42];
+//  memcpy_P(buff, font[i], 42);
+//  lcd.writeBitmap(buff, x, 2, 14, 3);
+//}
 
-void lcd_down() {
-  //lcd.stop();
-  digitalWrite(LCD_POWER, LOW);
-}
+//Get 2x10 thermometr logo PROGMEM bitmap.
+//void lcd_thermometr(){
+//  uint8_t buff[20];
+//  memcpy_P(buff, thermo, 20);
+//  lcd.writeBitmap(buff, 74, 2, 10, 2);
+//}
 
-void arduino_down() {
-  //sleep 10 min.
-  for (int i = 0; i < 75; i++) {
-    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
-  }
-}
+//Display server/client voltage reference.
+//void lcd_display_voltage(float server_vcc, float client_vcc) {
+//  lcd.print(server_vcc);
+//  lcd.gotoXY(18, 0);
+//  lcd.print("V");
+//  lcd.gotoXY(60, 0);
+//  lcd.print(client_vcc);
+//  lcd.gotoXY(78, 0);
+//  lcd.print("V");
+//}
+
+//Display temperature digits.
+//void lcd_display_temperature(float f) {
+//  if (f < 0) { lcd_font(10,0); } // "Negative One" ;).. 
+//  if (abs(int(f) / 10) > 0) { lcd_font(abs(int(f) / 10), 15); }
+//  lcd_font(abs(int(f) % 10), 30);
+//  lcd_font(11, 43); //Precision point.
+//  lcd_font(abs(int(f * 10.0) % 10), 56);
+//  lcd_thermometr();
+//}
+
+//Display error.
+//void lcd_display_error() {
+//  lcd_font(12, 56);
+//  lcd_thermometr();
+//}
+
+//void sleep(){
+//  LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);  
+//}
 
